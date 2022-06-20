7f79e42058f01740349bb09b614fbcecdca176fa
samiabat <samuelabatneh20@gmail.com>
2022-06-20 11:13:02 +0300

    add the front end

---
+++ myapp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
+import { LoginComponent } from './login/login.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterUserComponent,
+    LoginComponent,
+    WelcomePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
